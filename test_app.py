b85182caf71a04919d9a6c99f12580fdb5d547c1
shayan <shayan.naushad@gmail.com>
2026-02-02 14:11:48 +0500

    updating test_app.py

--- test_app.py
+++ test_app.py
@@ -1,2 +1,9 @@
-def test_example():
-    assert 1 + 1 == 2
+# test_app.py
+from app import some_function  # if you have functions in app.py
+
+def test_addition():
+    assert 2 + 3 == 5
+
+def test_failure():
+    assert 1 + 1 == 3  # This will fail and break CI
+
